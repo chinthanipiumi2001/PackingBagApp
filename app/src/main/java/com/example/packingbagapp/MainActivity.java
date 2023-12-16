fc18c8da4a3e5a5a335ee368cf3e71b3fb2f6bc7
user <dissanayakepiumi75@gmail.com>
2023-12-16 23:32:51 +0530

    add titles twelve times

--- app/src/main/java/com/example/packingbagapp/MainActivity.java
+++ app/src/main/java/com/example/packingbagapp/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.packingbagapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.packingbagapp.Adapter.Adapter;
+import com.example.packingbagapp.Constance.MyConstants;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<String> titles;
+    List<Integer> images;
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+    private void addAddTitles(){
+        titles = new ArrayList<>();
+        titles.add(MyConstants.BASIC_NEEDS_CAMEL_CASE);
+        titles.add(MyConstants.CLOTHING_CAMEL_CASE);
+        titles.add(MyConstants.PERSONAL_CARE_CAMEL_CASE);
+        titles.add(MyConstants.BABY_NEEDS_CAMEL_CASE);
+        titles.add(MyConstants.HEALTH_CAMEL_CASE);
+        titles.add(MyConstants.TECHNOLOGY_CAMEL_CASE);
+        titles.add(MyConstants.FOOD_CAMEL_CASE);
+        titles.add(MyConstants.BEACH_SUPPLIES_CAMEL_CASE);
+        titles.add(MyConstants.CAR_SUPPLIES_CAMEL_CASE);
+        titles.add(MyConstants.NEEDS_CAMEL_CASE);
+        titles.add(MyConstants.MY_LIST_CAMEL_CASE);
+        titles.add(MyConstants.MY_SELECTIONS_CAMEL_CASE);
+
+
     }
 }
