2a077cf381873a9b7aa14bb2c1b230b36bce9bdc
user <dissanayakepiumi75@gmail.com>
2023-12-20 16:15:15 +0530

    add string

--- app/src/main/java/com/example/packingbagapp/Models/Items.java
+++ app/src/main/java/com/example/packingbagapp/Models/Items.java
@@ -1,5 +1,6 @@
 package com.example.packingbagapp.Models;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
@@ -9,4 +10,27 @@ public class Items implements Serializable {
 
     @PrimaryKey(autoGenerate = true)
     int ID =0;
+
+
+    @ColumnInfo(name = "itemname")
+    String itemname;
+
+    @ColumnInfo(name="category")
+    String category;
+
+    @ColumnInfo(name = "addedby")
+    String addedby;
+
+    @ColumnInfo(name = "checked")
+    Boolean checked = false;
+
+    public Items(){
+
+    }
+    public Items(String itemname,String category,String addedby,Boolean checked){
+        this.itemname = itemname;
+        this.category = category;
+        this.addedby = addedby;
+        this.checked = checked;
+    }
 }
