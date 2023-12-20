c828d55463f1ab38b48a37554d345d645281f690
user <dissanayakepiumi75@gmail.com>
2023-12-21 00:26:39 +0530

    today final work

--- app/src/main/java/com/example/packingbagapp/Database/RoomDB.java
+++ app/src/main/java/com/example/packingbagapp/Database/RoomDB.java
@@ -1,4 +1,22 @@
 package com.example.packingbagapp.Database;
 
-public class RoomDB {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.example.packingbagapp.Models.Items;
+
+@Database(entities = Items.class,version = 1,exportSchema = false)
+public abstract class RoomDB extends RoomDatabase {
+
+    private static RoomDB database;
+    private static String DATABASE_NAME ="MyDb";
+
+    public synchronized static RoomDB getInstance(Context context){
+        if (database == null){
+
+        }
+        return database;
+    }
 }
