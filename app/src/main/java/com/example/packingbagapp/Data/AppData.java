baab79b827c552db6d3843c27a5272f16a91c98e
user <dissanayakepiumi75@gmail.com>
2023-12-22 00:55:07 +0530

    add version

--- app/src/main/java/com/example/packingbagapp/Data/AppData.java
+++ app/src/main/java/com/example/packingbagapp/Data/AppData.java
@@ -2,5 +2,14 @@ package com.example.packingbagapp.Data;
 
 import android.app.Application;
 
+import com.example.packingbagapp.Database.RoomDB;
+
 public class AppData extends Application {
+
+    RoomDB database;
+    String category;
+
+    public static final String LAST_VERSION = "LAST_VERSION";
+    public static final int NEW_VERSION = 3;
+
 }
