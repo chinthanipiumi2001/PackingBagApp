12796ed9611a93bd64dd5a3cf2f9c27535f4fb10
user <dissanayakepiumi75@gmail.com>
2024-01-01 23:28:26 +0530

    add null value

--- app/src/main/java/com/example/packingbagapp/Adapter/CheckListAdapter.java
+++ app/src/main/java/com/example/packingbagapp/Adapter/CheckListAdapter.java
@@ -1,15 +1,18 @@
 package com.example.packingbagapp.Adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.packingbagapp.Constance.MyConstants;
 import com.example.packingbagapp.Database.RoomDB;
 import com.example.packingbagapp.Models.Items;
 import com.example.packingbagapp.R;
@@ -26,14 +29,30 @@ public class CheckListAdapter extends RecyclerView.Adapter<CheckListViewHolder> 
     public CheckListAdapter() {
     }
 
+    public CheckListAdapter(Context context, List<Items> itemsList, RoomDB database, String show) {
+        this.context = context;
+        this.itemsList = itemsList;
+        this.database = database;
+        this.show = show;
+        if(itemsList.size()==0)
+            Toast.makeText(context.getApplicationContext(), "Nothing to show", Toast.LENGTH_SHORT).show();
+    }
+
     @NonNull
     @Override
     public CheckListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new CheckListViewHolder(LayoutInflater.from(context).inflate(R.layout.check_list_item,parent,false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull CheckListViewHolder holder, int position) {
+        holder.checkBox.setText(itemsList.get(position).getItemname());
+        holder.checkBox.setChecked(itemsList.get(position).getChecked());
+
+        if(MyConstants.FALSE_STRING.equals(show)){
+            holder.btnDelete.setVisibility(View.GONE);
+
+        }
 
     }
 
