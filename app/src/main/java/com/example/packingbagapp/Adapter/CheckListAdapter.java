17d5cbf98dba5d98a2561be2eeb2c9fb95cf2aaa
user <dissanayakepiumi75@gmail.com>
2024-01-01 20:24:41 +0530

    add constructors

--- app/src/main/java/com/example/packingbagapp/Adapter/CheckListAdapter.java
+++ app/src/main/java/com/example/packingbagapp/Adapter/CheckListAdapter.java
@@ -1,6 +1,8 @@
 package com.example.packingbagapp.Adapter;
 
+import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
@@ -8,9 +10,37 @@ import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.packingbagapp.Database.RoomDB;
+import com.example.packingbagapp.Models.Items;
 import com.example.packingbagapp.R;
 
-public class CheckListAdapter {
+import java.util.List;
+
+public class CheckListAdapter extends RecyclerView.Adapter<CheckListViewHolder> {
+
+    Context context;
+    List<Items> itemsList;
+    RoomDB database;
+    String show;
+
+    public CheckListAdapter() {
+    }
+
+    @NonNull
+    @Override
+    public CheckListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CheckListViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
 class CheckListViewHolder extends RecyclerView.ViewHolder{
 
